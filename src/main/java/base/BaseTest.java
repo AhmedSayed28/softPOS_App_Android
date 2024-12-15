da20095255fe228fcf0f061e53c8bdfeb501a90d
Ahmed Ghaly <ahm.say14@gmail.com>
2024-12-15 21:20:24 +0200

    finished the BaseTest class

---
+++ src/main/java/base/BaseTest.java
@@ -0,0 +1,37 @@
+package base;
+
+import driver.AppFactory;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+public class BaseTest {
+
+    @BeforeClass
+    public void launchApp() throws MalformedURLException {
+        System.out.println("before method");
+        AppFactory.launchApp();
+    }
+
+    @AfterMethod
+    public void closeApp(ITestResult result) throws IOException {
+        if(result.getStatus() == ITestResult.FAILURE){
+            Util.getScreenshot(result.getTestName());
+        }
+    }
+
+    @BeforeSuite
+    public void serverStart(){
+        System.out.println("before suite");
+    }
+
+    @AfterSuite
+    public void serverStop(){
+        AppiumServer.stop();
+    }
+}
